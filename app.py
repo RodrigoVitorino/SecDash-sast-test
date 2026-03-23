0bc7966a4aeed5dfb8dc6fbfaafead6b199e97ec
RodrigoVitorino <101281066+RodrigoVitorino@users.noreply.github.com>
2026-03-23 16:30:51 +0000

    Refactor app.py to try to cause security alerts

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-import sys
+from flask import Flask, request
 import sqlite3
 import subprocess
 
+app = Flask(__name__)
+
 # SQL Injection
-conn = sqlite3.connect("users.db")
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM users WHERE username = '" + sys.argv[1] + "'")
+@app.route('/user')
+def get_user():
+    username = request.args.get('username')
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = '" + username + "'")
+    return str(cursor.fetchall())
 
 # Command Injection
-subprocess.run("ls " + sys.argv[2], shell=True)
+@app.route('/ping')
+def ping():
+    host = request.args.get('host')
+    result = subprocess.run("ping -c 1 " + host, shell=True, capture_output=True, text=True)
+    return result.stdout
 
 # Code Injection
-eval(sys.argv[3])
+@app.route('/eval')
+def run_eval():
+    code = request.args.get('code')
+    return str(eval(code))
